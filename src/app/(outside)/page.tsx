772881d179c197b40c21b7c6f995249f8971866b
Pallavi <pallavilpmt1995@gmail.com>
2025-01-20 17:13:45 +0530

    Onboarding flow layout

--- src/app/(outside)/page.tsx
+++ src/app/(outside)/page.tsx
@@ -3,6 +3,7 @@ import { FeaturesSection } from "@/components/sections/FeaturesSection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { HowItWorksSection } from "@/components/sections/HowItWorksSection";
 import { NewsletterSection } from "@/components/sections/NewsletterSection";
+import {OnboardingForm} from "@/components/sections/OnboardingForm";
 import { PricingSection } from "@/components/sections/PricingSection";
 import { WhoCanUse } from "@/components/sections/WhoCanUse";
 
